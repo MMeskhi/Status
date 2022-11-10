0cce063abfaf259eaa8ac1b47aa83b2af3d4e59a
MMeskhi <mishameskhi1@gmail.com>
2022-11-10 16:09:43 +0400

    add main page

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+function heroSlider() {
+  const swiper = new Swiper(".hero_swiper", {
+    navigation: {
+      nextEl: ".hero_slider-next",
+      prevEl: ".hero_slider-prev",
+    },
+    slidesPerView: 1,
+    spaceBetween: 10,
+  });
+}
+function mainDoctorsSlider() {
+  const swiper = new Swiper(".main_doctors-swiper", {
+    breakpoints: {
+      320: {
+        slidesPerView: 1.4,
+        spaceBetween: 12,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 12,
+        grabCursor: true,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 16,
+      },
+    },
+  });
+}
+heroSlider();
+mainDoctorsSlider();
